fa60d0ac4c370e74eaf8f00c5d121d8c96cdbbc8
Maddie Wagner <musomaddy@gmail.com>
2022-04-08 21:00:25 +1000

    Improving javascript to make hiding / showing the classes a bit easier

--- game/static/scripts.js
+++ game/static/scripts.js
@@ -1,27 +1,30 @@
+var hiding_class = "hidden"
+
+function hideElement(value) {
+	// Hides the given element (by id) from the website by applying the css class
+	$(value).addClass(hiding_class)
+}
+
+function showElement(value) {
+	// Shows the given element (by id) by removing the CSS class.
+	// If this is already visible this shouldn't change anything
+	$(value).removeClass(hiding_class)
+}
+
+
 function dynamicResizing(width) {
 	var min_width_for_wide_title = 600
+	var wide_ids = ["#mainpage_wide", "#tt_1_wide", "#tt_2_wide"]
+	var tall_ids = ["#mainpage_tall", "#tt_1_tall", "#tt_2_tall"]
 
 	if (width < min_width_for_wide_title) {
-		console.log("tall image");
-		// Background images
-		$("#mainpage_wide").addClass("hidden")
-		$("#mainpage_tall").removeClass("hidden")
-
-		// Font sizes
-		$("#tt_1_wide").addClass("hidden")
-		$("#tt_2_wide").addClass("hidden")
-		$("#tt_1_tall").removeClass("hidden")
-		$("#tt_2_tall").removeClass("hidden")
+		// Tall version
+		wide_ids.forEach(hideElement);
+		tall_ids.forEach(showElement);
 	} else {
-		// Background images
-		$("#mainpage_wide").removeClass("hidden")
-		$("#mainpage_tall").addClass("hidden")
-
-		// Font sizes
-		$("#tt_1_tall").addClass("hidden")
-		$("#tt_2_tall").addClass("hidden")
-		$("#tt_1_wide").removeClass("hidden")
-		$("#tt_2_wide").removeClass("hidden")
+		// Wide version
+		wide_ids.forEach(showElement);
+		tall_ids.forEach(hideElement);
 	}
 }
 
