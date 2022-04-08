f50c002da47505dd89201ea8aa7c6e6e8f064b0b
Maddie Wagner <musomaddy@gmail.com>
2022-04-08 20:45:25 +1000

    Using tall instead of thin consistently

--- game/static/scripts.js
+++ game/static/scripts.js
@@ -1,26 +1,25 @@
 function dynamicResizing(width) {
 	var min_width_for_wide_title = 600
-	console.log("Width is");
-	console.log(width);
 
 	if (width < min_width_for_wide_title) {
+		console.log("tall image");
 		// Background images
 		$("#mainpage_wide").addClass("hidden")
-		$("#mainpage_thin").removeClass("hidden")
+		$("#mainpage_tall").removeClass("hidden")
 
 		// Font sizes
 		$("#tt_1_wide").addClass("hidden")
 		$("#tt_2_wide").addClass("hidden")
-		$("#tt_1_thin").removeClass("hidden")
-		$("#tt_2_thin").removeClass("hidden")
+		$("#tt_1_tall").removeClass("hidden")
+		$("#tt_2_tall").removeClass("hidden")
 	} else {
 		// Background images
 		$("#mainpage_wide").removeClass("hidden")
-		$("#mainpage_thin").addClass("hidden")
+		$("#mainpage_tall").addClass("hidden")
 
 		// Font sizes
-		$("#tt_1_thin").addClass("hidden")
-		$("#tt_2_thin").addClass("hidden")
+		$("#tt_1_tall").addClass("hidden")
+		$("#tt_2_tall").addClass("hidden")
 		$("#tt_1_wide").removeClass("hidden")
 		$("#tt_2_wide").removeClass("hidden")
 	}
