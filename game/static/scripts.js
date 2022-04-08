40654ec1dbb244cae2274f53cd0953588582c954
Maddie Wagner <musomaddy@gmail.com>
2022-04-08 13:00:03 +1000

    Added heading text for the wide screen.

--- game/static/scripts.js
+++ game/static/scripts.js
@@ -1,20 +1,36 @@
-// Load the images depending on the page resize.
-$(window).resize(function() {
-	if ($(this).width() <= 500) {
+function dynamicResizing(width) {
+	var min_width_for_wide_title = 600
+	console.log("Width is");
+	console.log(width);
+
+	if (width < min_width_for_wide_title) {
+		// Background images
 		$("#mainpage_wide").addClass("hidden")
 		$("#mainpage_thin").removeClass("hidden")
+
+		// Font sizes
+		$("#tt_1_wide").addClass("hidden")
+		$("#tt_2_wide").addClass("hidden")
+		$("#tt_1_thin").removeClass("hidden")
+		$("#tt_2_thin").removeClass("hidden")
 	} else {
+		// Background images
 		$("#mainpage_wide").removeClass("hidden")
 		$("#mainpage_thin").addClass("hidden")
+
+		// Font sizes
+		$("#tt_1_thin").addClass("hidden")
+		$("#tt_2_thin").addClass("hidden")
+		$("#tt_1_wide").removeClass("hidden")
+		$("#tt_2_wide").removeClass("hidden")
 	}
+}
+
+// Handle all the dynamic resizing options
+$(window).resize(function() {
+	dynamicResizing($(this).width());
 });
 
 window.onload = function() {
-	if ($(this).width() <= 500) {
-		$("#mainpage_wide").addClass("hidden")
-		$("#mainpage_thin").removeClass("hidden")
-	} else {
-		$("#mainpage_wide").removeClass("hidden")
-		$("#mainpage_thin").addClass("hidden")
-	}
+	dynamicResizing($(this).width());
 }
