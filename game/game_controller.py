a93be3197982f32c283d7b36d1a33c2322bb4ff5
Maddie Wagner <musomaddy@gmail.com>
2022-05-28 13:26:06 +1000

    Split site content (such as home and rules) into its own blueprint.

--- game/game_controller.py
+++ game/game_controller.py
@@ -1,7 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, request, render_template, url_for
 
 bp = Blueprint("game", __name__, url_prefix="/game")
 
 @bp.route("/", methods=("GET", "POST"))
-def start_game_page():
-    return render_template("game_menu.html")
+def join_or_start_game_page():
+    """ Manages the initial navigation to the game main page which contains two
+    choices: start a new game or join an existing one.
+    """
+    if request.method == "GET":
+        return render_template("game_menu.html")
+    if "new_game" in request.form:
+        return redirect(url_for("game.start_new_game"))
+    return redirect(url_for("game.join_game"))
+
+@bp.route("/start_new", methods=("GET",))
+def start_new_game():
+    """ Manages starting a new game. """
+    return render_template("game_start_new.html")
+
+@bp.route("/join", methods=("GET",))
+def join_game():
+    """ Manages joining an existing game. """
+    return render_template("game_join.html")
