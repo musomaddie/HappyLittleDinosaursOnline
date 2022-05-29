94c508ce21738ae1daffb8f5802680759ea42c2a
Madeleine Wagner <musomaddy@gmail.com>
2022-05-30 07:21:22 +1000

    Moved the logic to starting a socket on the client side whenever "game" is navigated to. I'm not convinced this is best but we'll see.

--- game/static/game_socket_handler.js
+++ game/static/game_socket_handler.js
@@ -1,6 +1,7 @@
 var socket = io();
 $(document).ready(function() {
-	socket = io.connect("http://" + document.domain + ":" + location.port + "/chat");
+	// TODO: update the connection name.
+	socket = io.connect("http://" + document.domain + ":" + location.port + "/aaaaaaaaaaaaaaaaaa");
 	socket.on("connect", function() {
 		console.log("joined");
 		socket.emit("joined", {});
@@ -10,4 +11,9 @@ $(document).ready(function() {
 socket.on("my_response", function(msg, cb) {
 	console.log("I have receieved a message");
 	$("#log").append("<br>" + msg.data).html();
+});
+
+socket.on("disconnect", function() {
+	console.log("disconnected");
+	socket.emit("lost");
 });
